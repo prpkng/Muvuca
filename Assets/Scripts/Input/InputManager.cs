5c2ed678c5a53612a263208247773ee6248d2955
Purpkin <guiejo2020@gmail.com>
2024-05-05 14:13:19 -0300

    Basic mobile platform support

--- Assets/Scripts/Input/InputManager.cs
+++ Assets/Scripts/Input/InputManager.cs
@@ -18,20 +18,35 @@ public class InputManager : MonoBehaviour
     public static Action JumpPressed;
     public static Action AttackPressed;
 
+    private static Vector2 TouchPos = Vector2.zero;
+
     private void OnActionTriggered(InputAction.CallbackContext obj)
     {
-        if (!obj.started) return;
         switch (obj.action.name)
         {
             case "Jump":
+                if (!obj.started) return;
                 JumpPressed?.Invoke();
                 break;
             case "Attack":
+                if (!obj.started) return;
                 AttackPressed?.Invoke();
                 break;
             case "Reset":
+                if (!obj.started) return;
                 LevelManager.Reset();
                 break;
+            case "TouchPos":
+                if (!GameManager.isMobilePlatform) return;
+                TouchPos = obj.ReadValue<Vector2>();
+                var size = Screen.width;
+                var left = TouchPos.x < size / 2;
+                if (left) // LEFT CLICK
+                    JumpPressed?.Invoke();
+                else                       // RIGHT CLICK
+                    AttackPressed?.Invoke();
+                break;
+
         }
     }
 }
---
+++ Assets/Scripts/Systems/ReadOnlyAttribute.cs
@@ -0,0 +1,25 @@
+
+
+using UnityEngine;
+
+#if UNITY_EDITOR
+
+using UnityEditor;
+
+public class ReadOnlyEditor : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        GUI.enabled = false;
+        base.OnGUI(position, property, label);
+        GUI.enabled = true;
+    }
+}
+
+#endif
+
+
+public class ReadOnlyAttribute : PropertyAttribute
+{
+
+}
