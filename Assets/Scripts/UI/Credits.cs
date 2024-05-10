b7e97014a720f3c1418493c590ad9620bd8241c0
Purpkin <guiejo2020@gmail.com>
2024-05-09 22:17:55 -0300

    Tela de créditos (traduzida) :D

---
+++ Assets/Scripts/UI/Credits.cs
@@ -0,0 +1,35 @@
+namespace Muvuca.UI
+{
+    using TMPro;
+    using UnityEngine;
+
+    public class Credits : MonoBehaviour
+    {
+        public string programming;
+        public void SetProgramming(string text) => programming = text;
+        public string artist2D;
+        public void SetArtist2D(string text) => artist2D = text;
+        public string animator;
+        public void SetAnimator(string text) => animator = text;
+        public string soundDesign;
+        public void SetSoundDesign(string text) => soundDesign = text;
+        public string gameDesign;
+        public void SetGameDesign(string text) => gameDesign = text;
+
+
+
+        private TMP_Text tmp;
+        private void Awake() => tmp = GetComponent<TMP_Text>();
+
+        public void UpdatedText()
+        {
+            tmp.text = $@"Guilherme Silva - {programming}
+Bruno Nascimento - UI/UX
+Samuel Figueiredo - {soundDesign} / OST
+Brendo Saldanha - {artist2D} & {gameDesign}
+Gabriel Dias - {artist2D}
+Murilo Malsan - {animator}";
+        }
+
+    }
+}
