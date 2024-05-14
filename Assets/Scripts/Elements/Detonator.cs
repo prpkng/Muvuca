4b3b0b5fbe281477914c6d3c647c58c8a6088a5b
Purpkin <guiejo2020@gmail.com>
2024-05-14 19:47:59 -0300

    Simple detonator animation

---
+++ Assets/Scripts/Elements/Detonator.cs
@@ -0,0 +1,54 @@
+using System;
+using DG.Tweening;
+using Muvuca.Input;
+using Muvuca.Systems;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace Muvuca.Elements
+{
+    public class Detonator : MonoBehaviour
+    {
+        [SerializeField] private Transform detonatorTransform;
+        [SerializeField] private float shockwaveStartSize;
+        [SerializeField] private float shockwaveDestSize;
+        [SerializeField] private float shockwaveDuration;
+        [SerializeField] private Ease shockwaveEase;
+        private HitboxChecker checker;
+        private void Awake() => checker = GetComponent<HitboxChecker>();
+
+        private void OnEnable()
+        {
+            checker.entered += Entered;
+            checker.exited += Exited;
+        }
+        private void OnDisable()
+        {
+            checker.entered -= Entered;
+            checker.exited -= Exited;
+        }
+
+        private AsyncOperationHandle<Material>? shockwaveMaterial;
+        
+        private void Entered()
+        {
+            InputManager.AttackPressed += ActivateDetonator;
+            shockwaveMaterial = Addressables.LoadAssetAsync<Material>("Assets/Art/Materials/Shockwave");
+        }
+
+        private void Exited()
+        {
+            InputManager.AttackPressed -= ActivateDetonator;
+            shockwaveMaterial = null;
+        }
+
+        private async void ActivateDetonator()
+        {
+            shockwaveMaterial?.Result.SetFloat("_Size", shockwaveStartSize);
+            shockwaveMaterial?.Result.DOFloat(shockwaveDestSize, "_Size", shockwaveDuration).SetEase(shockwaveEase);
+            Destroy(this);
+            Destroy(detonatorTransform.gameObject);
+        }
+    }
+}
